5b4bd9cba94b520d666edd8b753bfb819120f9f9
ThuanB1910150 <thuankg211@gmail.com>
2024-09-20 19:06:25 +0700

    Dinh nghia ca route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,5 +13,7 @@ app.get("/", (req, res) => {
         message: "Welcome to contact book application."
     });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
